79efccfe2d6ff5e89b7616d041cd5f05b791b7d4
Samuel Marchildon-Lavoie <samuel.m.lavoie@gmail.com>
2016-03-02 11:28:02 -0500

    Return an exception when an error is throw

--- src/main/java/io/github/djxy/javascript/models/JSONParser.java
+++ src/main/java/io/github/djxy/javascript/models/JSONParser.java
@@ -6,7 +6,6 @@ import org.json.JSONObject;
 
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
 
 /**
  * Created by Samuel on 2016-02-18.
@@ -15,29 +14,15 @@ public class JSONParser {
 
     private final static ScriptEngine scriptEngine = new ScriptEngineManager().getEngineByName("nashorn");
 
-    public static Object parse(String json){
-        try {
-            return scriptEngine.eval("Java.asJSONCompatible("+json+")");
-        } catch (ScriptException e) {}
-
-        return null;
+    public static Object parse(String json) throws Exception {
+        return scriptEngine.eval("Java.asJSONCompatible("+json+")");
     }
 
     public static String stringify(ScriptObjectMirror object){
-
-        if(!object.isArray()) {
-            try {
-                return new JSONObject(object).toString(4);
-            } catch (Exception e) {
-            }
-        }
-        else{
-            try {
-                return new JSONArray(object.values()).toString(4);
-            }catch(Exception e){}
-        }
-
-        return "";
+        if(!object.isArray())
+            return new JSONObject(object).toString(4);
+        else
+            return new JSONArray(object.values()).toString(4);
     }
 
 }
