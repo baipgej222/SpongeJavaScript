5ce0360c4aee15b5281932e9bda55de4b841cb8a
Samuel Marchildon-Lavoie <samuel.m.lavoie@gmail.com>
2016-03-19 12:39:02 -0400

    Added global variables.

--- src/main/java/io/github/djxy/spongejavascript/script/SpongeScriptManager.java
+++ src/main/java/io/github/djxy/spongejavascript/script/SpongeScriptManager.java
@@ -14,22 +14,13 @@ import java.util.ArrayList;
 public class SpongeScriptManager extends ScriptManager {
 
     public SpongeScriptManager() {
+        super();
         addVariable("game", Sponge.getGame());
 
-        addCode("var Javascript = Java.type('io.github.djxy.spongejavascript.javascript.JavascriptObject');");
-        addCode("var JSON = Java.type('io.github.djxy.spongejavascript.script.util.JSONParser');");
-        addCode("var Text = Java.type('org.spongepowered.api.text.Text');");
-        addCode("var TextSerializers = Java.type('org.spongepowered.api.text.serializer.TextSerializers');");
-        addCode("var TextColors = Java.type('org.spongepowered.api.text.format.TextColors');");
-        addCode("var Player = Java.type('org.spongepowered.api.entity.living.player.Player');");
-        addCode("function setInterval(callback, interval){return scheduler.setInterval(callback, interval);}");
-        addCode("function setTimeout(callback, delay){return scheduler.setTimeout(callback, delay);}");
-        addCode("function clearInterval(intervalId){Scheduler.getInstance().clearInterval(intervalId);}");
-        addCode("function clearTimeout(timeoutId){Scheduler.getInstance().clearTimeout(timeoutId);}");
-        addCode("function convertToJSObject(object){return Javascript.convertObjectToJSObject(object);}");
-        addCode("function convertToObject(object){return Javascript.convertJSObjectToObject(object);}");
-        addCode("function stringToText(text){return TextSerializers.FORMATTING_CODE.deserialize(text);}");
-
+        addCode(SCHEDULER_FUNCTIONS);
+        addCode(JAVASCRIPT);
+        addCode(JSON);
+        addCode(TEXT);
     }
 
     @Override
@@ -46,23 +37,19 @@ public class SpongeScriptManager extends ScriptManager {
     }
 
     public void onGameConstructionEvent(GameConstructionEvent event){
-        for(Script script : getScripts())
-            script.invoke("onGameConstructionEvent", event);
+        invoke("onGameConstructionEvent", event);
     }
 
     public void onGamePreInitializationEvent(GamePreInitializationEvent event){
-        for(Script script : getScripts())
-            script.invoke("onGamePreInitializationEvent", event);
+        invoke("onGamePreInitializationEvent", event);
     }
 
     public void onGameInitializationEvent(GameInitializationEvent event){
-        for(Script script : getScripts())
-            script.invoke("onGameInitializationEvent", event);
+        invoke("onGameInitializationEvent", event);
     }
 
     public void onGamePostInitializationEvent(GamePostInitializationEvent event){
-        for(Script script : getScripts())
-            script.invoke("onGamePostInitializationEvent", event);
+        invoke("onGamePostInitializationEvent", event);
 
         if(Sponge.getGame().getServiceManager().provide(org.spongepowered.api.service.economy.EconomyService.class).isPresent())
             for(Script script : getScripts())
@@ -70,33 +57,27 @@ public class SpongeScriptManager extends ScriptManager {
     }
 
     public void onGameLoadCompleteEvent(GameLoadCompleteEvent event){
-        for (Script script : getScripts())
-            script.invoke("onGameLoadCompleteEvent", event);
+        invoke("onGameLoadCompleteEvent", event);
     }
 
     public void onGameAboutToStartServerEvent(GameAboutToStartServerEvent event){
-        for (Script script : getScripts())
-            script.invoke("onGameAboutToStartServerEvent", event);
+        invoke("onGameAboutToStartServerEvent", event);
     }
 
     public void onGameStartingServerEvent(GameStartingServerEvent event){
-        for (Script script : getScripts())
-            script.invoke("onGameStartingServerEvent", event);
+        invoke("onGameStartingServerEvent", event);
     }
 
     public void onGameStartedServerEvent(GameStartedServerEvent event){
-        for (Script script : getScripts())
-            script.invoke("onGameStartedServerEvent", event);
+        invoke("onGameStartedServerEvent", event);
     }
 
     public void onGameStoppingServerEvent(GameStoppingServerEvent event){
-        for (Script script : getScripts())
-            script.invoke("onGameStoppingServerEvent", event);
+        invoke("onGameStoppingServerEvent", event);
     }
 
     public void onGameStoppedServerEvent(GameStoppedServerEvent event){
-        for (Script script : getScripts())
-            script.invoke("onGameStoppedServerEvent", event);
+        invoke("onGameStoppedServerEvent", event);
     }
 
 }
